8130cf5a38d76fa80754c786792e0d1268ce4610
anamaria61 <bernalzarateanamaria61@gmail.com>
2026-04-07 15:52:12 -0500

    Funciones

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Requerimientos funcionales
-// 1 Mostrar todos los aprendices con nombre, nota y programa. 
-// 2 Mostrar aprendices aprobados usando filter() con la condición nota >= 3.0. 
-// 3 Mostrar aprendices reprobados usando filter() con la condición nota < 3.0. 
-// 4 Transformar los nombres a mayúscula usando map(). 
-// 5 Calcular el promedio general usando reduce().  
+// 1 Mostrar todos los aprendices con nombre, nota y programa. -
+// 2 Mostrar aprendices aprobados usando filter() con la condición nota >= 3.0. -
+// 3 Mostrar aprendices reprobados usando filter() con la condición nota < 3.0. -
+// 4 Transformar los nombres a mayúscula usando map().  -
+// 5 Calcular el promedio general usando reduce().  -
 // 6 Ordenar los aprendices por nota de mayor a menor usando sort(). 
 // 7 Clasificar una nota con switch en niveles como bajo, básico, alto o superior. 
 // 8 Construir un menú repetitivo con while que permita ejecutar varias opciones hasta salir.
@@ -16,3 +16,26 @@ const aprendices = [
 	{ nombre: "Pedro", nota: 3.8, programa: "ADSO" }
 ];
 console.table(aprendices)
+
+const aprobados = aprendices.filter((aprendiz) => aprendiz.nota >= 3.0)
+console.log(aprobados)
+
+const reprobados = aprendices.filter((aprendiz) => aprendiz.nota < 3.0)
+console.log(reprobados)
+
+///TRANFORMAR A MAYUSCULAS///
+const nom_modificados = aprendices.map( aprendiz => aprendiz.nombre.toUpperCase());
+console.log(nom_modificados)
+
+//FUNCIÓN CALCULAR PROMEDIO///
+
+function calcularPromedio() {
+    const suma = aprendices.reduce((acc, aprendiz) => acc + aprendiz.nota, 0);
+    return (suma / aprendices.length).toFixed(2);
+}
+console.log("El promedio de los estudiantes es: " + calcularPromedio())
+
+//ORDEN DE NOTAS//
+const orden_nota = 
+    [...aprendices].sort((a, b) => b.nota - a.nota, 0);
+console.log(orden_nota)
