9237c8947ff8b999dc6194bbebd6adc661f4a3cc
crissand16 <cristian.a.gomez16@gmail.com>
2026-04-12 22:13:41 -0500

    Cargando todo a rama main

---
+++ index.js
@@ -0,0 +1,121 @@
+// Requerimientos funcionales
+// 1 Mostrar todos los aprendices con nombre, nota y programa. -
+// 2 Mostrar aprendices aprobados usando filter() con la condición nota >= 3.0. -
+// 3 Mostrar aprendices reprobados usando filter() con la condición nota < 3.0. -
+// 4 Transformar los nombres a mayúscula usando map().  -
+// 5 Calcular el promedio general usando reduce().  -
+// 6 Ordenar los aprendices por nota de mayor a menor usando sort(). -
+// 7 Clasificar una nota con switch en niveles como bajo, básico, alto o superior. -
+// 8 Construir un menú repetitivo con while que permita ejecutar varias opciones hasta salir.-
+
+const prompt = require("prompt-sync")();
+
+
+const aprendices = [
+	{ nombre: "Pepito", nota: 3.0, programa: "ADSO" },
+	{ nombre: "Alejandra", nota: 4.5, programa: "Logística" },
+	{ nombre: "Manuel", nota: 4.0, programa: "ADSO" },
+	{ nombre: "Juana", nota: 2.8, programa: "Logística" },
+	{ nombre: "Pedro", nota: 3.8, programa: "ADSO" }
+];
+console.table(aprendices)
+
+const aprobados = aprendices.filter((aprendiz) => aprendiz.nota >= 3.0)
+console.log(aprobados)
+
+const reprobados = aprendices.filter((aprendiz) => aprendiz.nota < 3.0)
+console.log(reprobados)
+
+///TRANFORMAR A MAYUSCULAS///
+const nom_modificados = aprendices.map( aprendiz => aprendiz.nombre.toUpperCase());
+console.log(nom_modificados)
+
+//FUNCIÓN CALCULAR PROMEDIO///
+
+function calcularPromedio() {
+    const suma = aprendices.reduce((acc, aprendiz) => acc + aprendiz.nota, 0);
+    return (suma / aprendices.length).toFixed(2);
+}
+console.log("El promedio de los estudiantes es: " + calcularPromedio())
+
+//ORDEN DE NOTAS//
+const orden_nota = 
+    [...aprendices].sort((a, b) => b.nota - a.nota, 0);
+console.log(orden_nota)
+
+
+//FUNCION PARA CLASIFICAR LA NOTA
+function clasificarNota(nota) {
+	switch (true){
+		case (nota < 3): return "Bajo";
+		case (nota < 4): return "Basico";
+		case (nota < 4.5): return "Alto";
+		default: return "Superior";
+	}
+}
+
+const tablaClasf = aprendices.map(aprendiz => ({
+	nombre: aprendiz.nombre,
+	nota: aprendiz.nota,
+	clasificacion: clasificarNota(aprendiz.nota)
+}))
+
+console.table(tablaClasf)
+
+//FUNCION MENU REPETITIVO CON WHILE
+
+function clasificarNota1(nota) {
+	switch (true){
+		case (nota < 3): return "Bajo";
+		case (nota < 4): return "Basico";
+		case (nota < 4.5): return "Alto";
+		default: return "Superior";
+	}
+}
+
+let opcion = 0
+
+while (opcion !== 4) {
+	console.log("\n ----MENU-----");
+	console.log("1. Ver Aprendices");
+	console.log("2. Ver clasificacion");
+	console.log("3. Buscar Aprendiz por Nombre");
+	console.log("4. Salir del menu");
+
+	opcion = Number(prompt("Elige una opcion: "));
+
+	switch (opcion) {
+		case 1:
+			console.table(aprendices);
+			break;
+
+		case 2:
+			const tabla = aprendices.map(a => ({
+				nombre: a.nombre,
+				nota: a.nota,
+				clasificacion: clasificarNota1(a.nota)
+			})); 
+			console.table(tabla)
+			break;
+
+		case 3:
+			const nombre = prompt ("Ingrese el nombre: ");
+			const encontrado = aprendices.find(a =>
+				a.nombre.toLowerCase() === nombre.toLowerCase()
+			);
+
+			if (encontrado) {
+				console.table([encontrado]);
+			} else {
+				console.log("No encontrado");
+			}
+			break;
+
+		case 4: 
+			console.log("Saliendo...");
+			break;
+
+		default:
+			console.log("Opcion Invalida")
+	}
+}
